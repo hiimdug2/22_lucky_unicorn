15c0a2cca18f58084360f0fd4c3a2e5068244313
hiimdug2 <strick.jorel@awatapu.school.nz>
2022-05-02 10:56:17 +1200

    made yes or no work

--- 01_yes_no_v3.py
+++ 01_yes_no_v3.py
@@ -18,16 +18,5 @@ def yes_no_checker(question):
             print("you must enter either yes or no")
 
 
-asking = True
-
-while asking:
-    show_instructions = yes_no_checker("have you played before ")
-
-    if show_instructions == "yes":
-        print("you have chosen not to see the instructions")
-
-    else:
-        show_instructions = "no"
-        print("you have chosen to see the instructions")
-
-    asking = False
+show_instructions = yes_no_checker("do you want to see the instructions ")
+print(f"you chose {show_instructions}")
